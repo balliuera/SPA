ecf9ecb4298bfbc9bd4e0e3d59c8ad4afb79ef18
Ilda Balliu <ildaballiu09@gmail.com>
2016-06-01 21:29:34 +0200

    initial commit

---
+++ controllers.js
@@ -0,0 +1,18 @@
+
+
+
+   app.controller('Homepage', function($scope){
+       $scope.homepage = "Homepage";
+   });
+
+   app.controller('About', function($scope){
+       $scope.about = "Lorem ipsum...";
+   });
+
+   app.controller('Date', function($scope) {
+       $scope.now = new Date();
+   });
+
+
+
+
